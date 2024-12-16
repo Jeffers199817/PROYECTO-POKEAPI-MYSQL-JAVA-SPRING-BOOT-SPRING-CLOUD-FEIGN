f9b778363b0fb7cf991baa511bd18804acba824d
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-16 16:49:21 -0500

    3. Implementación de la interfaz IpokeApi y creación de endpoint para consumir la url de @FeignClient

--- src/main/java/com/milenyumsoft/pokeapi/dto/PokemonDTO.java
+++ src/main/java/com/milenyumsoft/pokeapi/dto/PokemonDTO.java
@@ -1,4 +1,18 @@
 package com.milenyumsoft.pokeapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class PokemonDTO {
+
+    private int id;
+    private String name;
+
 }
